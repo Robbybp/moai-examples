936a3a337c4ea6a122ed5d95cd5c5e858a02e65d
Robert Parker <robbybparker@gmail.com>
2025-04-16 20:36:00 -0400

    get matrix

---
+++ mnist/small-model.jl
@@ -0,0 +1,21 @@
+import JuMP
+import MathOptAI as MOAI
+
+function make_small_model(; relaxation_parameter = 1e-6)
+    m = JuMP.Model()
+    JuMP.@variable(m, x[1:input_dim] >= 0)
+
+    A1 = rand(hidden_dim, input_dim)
+    b1 = rand(hidden_dim)
+    A2 = rand(output_dim, hidden_dim)
+    b2 = rand(output_dim)
+    predictor = MOAI.Pipeline(
+        MOAI.Affine(A1, b1),
+        MOAI.ReLUQuadratic(; relaxation_parameter = relaxation_parameter),
+        MOAI.Affine(A2, b2),
+        MOAI.SoftMax(),
+    )
+    y, formulation = MOAI.add_predictor(m, predictor, x)
+    JuMP.@objective(m, Min, sum(x.^2) + sum(y.^2))
+    return m, formulation
+end
--- mnist/test-small.jl
+++ mnist/test-small.jl
@@ -16,23 +16,8 @@ optimizer = JuMP.optimizer_with_attributes(
     "linear_solver" => MadNLPHSL.Ma27Solver,
 )
 
-m = JuMP.Model()
-#m = JuMP.direct_model(optimizer)
-JuMP.@variable(m, x[1:input_dim] >= 0)
-
-A1 = rand(hidden_dim, input_dim)
-b1 = rand(hidden_dim)
-A2 = rand(output_dim, hidden_dim)
-b2 = rand(output_dim)
-predictor = MOAI.Pipeline(
-    MOAI.Affine(A1, b1),
-    MOAI.ReLUQuadratic(; relaxation_parameter = 1e-6),
-    MOAI.Affine(A2, b2),
-    MOAI.SoftMax(),
-)
-y, formulation = MOAI.add_predictor(m, predictor, x)
-JuMP.@objective(m, Min, sum(x.^2) + sum(y.^2))
-
+include("small-model.jl")
+m, formulation = make_small_model()
 include("linalg.jl")
 
 solve = false
@@ -62,5 +47,52 @@ index_map, nvar, lvar, uvar, x0 = NLPModelsJuMP.parser_variables(moimodel)
 
 # This populates index_map with the correct indices
 NLPModelsJuMP.parser_MOI(moimodel, index_map, nvar)
+# - Gets constraint types, iterates over them
+# - Gets constraint indices of the given type from `backend`
+# - get the constraint function for this index
+# - index-map maps original index (in backend) to new index
+
 #nlp_data = NLPModelsJuMP._nlp_block(moimodel)
 #NLPModelsJuMP.parser_NL(nlp_data, hessian = true)
+
+include("formulation.jl")
+nn_vars, nn_cons = get_vars_cons(formulation)
+
+include("nlpmodels.jl")
+# These are the var/con orders in the MathOptNLPModel.
+varorder, conorder = get_var_con_order(m)
+ScalarFunction = Union{
+    MOI.ScalarAffineFunction,
+    MOI.ScalarQuadraticFunction,
+    MOI.ScalarNonlinearFunction,
+}
+# If we have vector constraints, we have to do something fancier to get the coordinates
+@assert all([MOI.get(m, MOI.ConstraintFunction(), c) isa ScalarFunction for c in conorder])
+var_idx_map = Dict{JuMP.VariableRef,Int}(var => i for (i, var) in enumerate(varorder))
+con_idx_map = Dict{JuMP.ConstraintRef,Int}(con => i for (i, con) in enumerate(conorder))
+
+nn_vindices = [var_idx_map[v] for v in nn_vars]
+nn_cindices = [con_idx_map[c] for c in nn_cons]
+
+# Apply offset of nvar+nslack to get constraints' locations in the KKT system
+nvar = length(varorder)
+ind_cons = MadNLP.get_index_constraints(nlp)
+nslack = length(ind_cons.ind_ineq)
+
+# Get the coordinates of NN vars/cons in the lower-left Jacobian
+# Columns (variables)
+nn_kktcols = nn_vindices
+# Rows (constraints) are offset by the size of the Hessian block
+nn_kktrows = nn_cindices .+ (nvar + nslack)
+
+cb = MadNLP.create_callback(MadNLP.SparseCallback, nlp)
+linear_solver = SchurComplementSolver
+kkt = MadNLP.create_kkt_system(
+    MadNLP.SparseKKTSystem,
+    cb,
+    ind_cons,
+    linear_solver,
+)
+MadNLP.initialize!(kkt)
+kkt_matrix = MadNLP.get_kkt(kkt)
+submatrix = kkt_matrix[nn_kktrows, nn_kktcols]
