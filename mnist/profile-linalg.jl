7bfe0c9e81d2fdef1728dc6eabbb22d1f52af596
Robert Parker <robbybparker@gmail.com>
2025-05-21 21:16:11 -0400

    check columns of B

--- mnist/profile-linalg.jl
+++ mnist/profile-linalg.jl
@@ -6,6 +6,7 @@ import MathProgIncidence
 import SparseArrays
 import Profile
 
+import Printf
 import DataFrames
 import CSV
 
@@ -15,29 +16,29 @@ include("formulation.jl")
 include("nlpmodels.jl")
 include("models.jl")
 
-function precompile_linalg(; schur=true)
+function precompile_linalg(; Solver=MadNLPHSL.Ma27Solver, schur=true)
     model, info = make_tiny_model()
     _, _, kkt_matrix = get_kkt(model)
     pivot_indices = get_kkt_indices(model, info.variables, info.constraints)
     pivot_indices = convert(Vector{Int32}, pivot_indices)
-    opt = SchurComplementOptions(; pivot_indices)
+    opt = SchurComplementOptions(; ReducedSolver=Solver, PivotSolver=Solver, pivot_indices)
     solver = SchurComplementSolver(kkt_matrix; opt)
     rhs = ones(kkt_matrix.m)
     MadNLP.factorize!(solver)
     MadNLP.solve!(solver, rhs)
     # TODO: Factorize with "baseline solver" in precompile section
 end
-    
+
 if false
     model, outputs, formulation = get_adversarial_model(
         nnfile, image_index, adversarial_label, threshold
     )
-    
+
     reduced_model, reduced_outputs, reduced_formulation = get_adversarial_model(
         nnfile, image_index, adversarial_label, threshold;
         reduced_space = true,
     )
-    
+
     nlp, kkt_system, kkt_matrix = get_kkt(model)
     pivot_vars, pivot_cons = get_vars_cons(formulation)
     pivot_indices = get_kkt_indices(model, pivot_vars, pivot_cons)
@@ -45,9 +46,9 @@ if false
     rhs = ones(kkt_matrix.m)
     opt = SchurComplementOptions(; pivot_indices)
     solver = SchurComplementSolver(kkt_matrix; opt)
-    
+
     display(kkt_matrix)
-    
+
     NSAMPLES = 1
     for i in 1:NSAMPLES
         d = copy(rhs)
@@ -75,6 +76,12 @@ function profile_solver(
     solver = Solver(kkt_matrix; opt)
     t_init = time() - t_init_start
 
+    # For better or worse, we can't call introduce until solver is initialized.
+    # This actually works well for Schur complement solvers because it means
+    # I can include subsolvers in the string (of course, I could already do this
+    # with multiple calls to `introduce`). But it's a little inconvenient here
+    # (I need to wait for initialization to see confirmation that I'm using the
+    # right solve).
     println(MadNLP.introduce(solver))
     println("-----------")
     println("initialize: $t_init")
@@ -89,13 +96,16 @@ function profile_solver(
 
     println("factorize:  $t_factorize")
     println("solve:      $t_solve")
-    # TODO: Return something
+    timer = (Solver === SchurComplementSolver) ? solver.timer : nothing
     info = (;
         time = (;
             initialize = t_init,
             factorize = t_factorize,
             solve = t_solve,
         ),
+        # There is some redundant information here, but this is easier than adding
+        # more fields to info.time.
+        timer,
         nnz = SparseArrays.nnz(kkt_matrix),
     )
     return info
@@ -111,6 +121,9 @@ function profile_solver(
     #adversarial_label = nothing,
     #threshold = nothing,
 )
+    if schur && reduced_space
+        error("schur=true && reduced_space=true doesn't make sense")
+    end
     formname = reduced_space ? "reduced-space" : "full-space"
     println("Profiling the $formname formulation")
     # TODO: Don't rely on global data here.
@@ -139,35 +152,60 @@ function profile_solver(
     newtimedata = merge((; model=t_model), info.time)
     result = (;
         time = newtimedata,
+        timer = info.timer,
         nnz = info.nnz,
     )
     return result
 end
 
 
-if abspath(PROGRAM_FILE) == @__FILE__
+#if abspath(PROGRAM_FILE) == @__FILE__
     # TODO: CLI
-    # Global data:
+    # Global data. Unfortunately, we rely on this in profile_solver(Solver, file)
     IMAGE_INDEX = 7
     ADVERSARIAL_LABEL = 1
     THRESHOLD = 0.6
 
     PRECOMPILE = true
     if PRECOMPILE
-        precompile_linalg()
+        precompile_linalg(; Solver=MadNLPHSL.Ma57Solver)
     end
 
     nnfile = joinpath("nn-models", "mnist-relu128nodes4layers.pt")
+    #nnfile = joinpath("nn-models", "mnist-relu1024nodes4layers.pt")
+    #nnfile = joinpath("nn-models", "mnist-relu2050nodes4layers.pt")
     model, outputs, formulation = get_adversarial_model(
-        nnfile, IMAGE_INDEX, ADVERSARIAL_LABEL, THRESHOLD
+        nnfile, IMAGE_INDEX, ADVERSARIAL_LABEL, THRESHOLD;
+        reduced_space = false
     )
-    nlp, kkt_system, kkt_matrix = get_kkt(model)
-    profile_solver(MadNLPHSL.Ma27Solver, kkt_matrix)
-    profile_solver(MadNLPHSL.Ma27Solver, nnfile; reduced_space = true)
+    nlp, kkt_system, kkt_matrix = get_kkt(model, Solver=MadNLPHSL.Ma57Solver)
+    #display(kkt_matrix)
+    #profile_solver(MadNLPHSL.Ma27Solver, kkt_matrix)
+    #profile_solver(MadNLPHSL.Ma27Solver, nnfile; reduced_space = true)
+    #results = profile_solver(MadNLPHSL.Ma57Solver, nnfile; schur = true)
+
+    pivot_vars, pivot_cons = get_vars_cons(formulation)
+    pivot_indices = get_kkt_indices(model, pivot_vars, pivot_cons)
+    pivot_index_set = Set(pivot_indices)
+    @assert kkt_matrix.m == kkt_matrix.n
+    reduced_indices = filter(i -> !(i in pivot_index_set), 1:kkt_matrix.m)
+    P = pivot_indices
+    R = reduced_indices
+    B = kkt_matrix[P, R] + kkt_matrix[R, P]'
+
+    # These are columns that have at least one entry
+    nzcols = map(i -> B.colptr[i] < B.colptr[i+1], 1:length(R))
+    nnzcols = count(nzcols)
+    nnzcol_percent = 100.0 * nnzcols / length(R)
+    nnzcol_percent = Printf.@sprintf("%1.1f", nnzcol_percent)
+    println("$nnzcols out of $(length(R)) columns have entries ($(nnzcol_percent)%)")
+
+    # TODO: We'll want this in a real data structure
+    #println(results.timer)
 
     PROFILE = false
     if PROFILE
         println("MadNLP.factorize!(::SchurComplementSolver) Profile:")
         Profile.print()
     end
-end
+#end
