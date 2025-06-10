5c2b7ef542c29403a42f942f61bc070f85d98d53
Robert Parker <robbybparker@gmail.com>
2025-06-10 00:03:05 -0400

    start test on jacobian

--- mnist/test-btsolver.jl
+++ mnist/test-btsolver.jl
@@ -1,7 +1,13 @@
+import JuMP
+import MadNLP
+import MadNLPHSL
+import NLPModels
 import SparseArrays
 using Test
 
 include("btsolver.jl")
+include("models.jl")
+include("nlpmodels.jl")
 
 function test_3x3_lt()
     matrix = [
@@ -45,7 +51,43 @@ function test_4x4_blt()
     return
 end
 
+function test_nn_jacobian()
+    # NOTE: This model is constructed with random numbers.
+    # TODO: Use a deterministic model here.
+    model, info = make_small_nn_model()
+    # ... do FixRef constraints not get picked up by NLPModels?
+    #JuMP.fix.(model[:x], 2.0; force = true)
+    JuMP.@constraint(model, model[:x] .== 2.0)
+    nlp = NLPModelsJuMP.MathOptNLPModel(model)
+    vars, cons = get_var_con_order(model)
+    x = [something(JuMP.start_value(var), 1.0) for var in vars]
+    matrix = NLPModels.jac(nlp, x)
+    display(matrix)
+
+    dim = matrix.m
+    nrhs = 10
+    rhs = ones(dim, nrhs)
+    btsolver = BlockTriangularSolver(matrix)
+    factorize!(btsolver)
+    sol = copy(rhs)
+    solve!(btsolver, sol)
+    display(sol)
+
+    baseline = matrix \ rhs
+    display(baseline)
+
+    matches = isapprox.(sol, baseline, atol=1e-8)
+    println("Discrepancies:")
+    display(matches)
+    nmatches = sum(matches)
+    println("Number of matches: $nmatches")
+
+    #@test all(isapprox.(sol, baseline, atol=1e-8))
+    return
+end
+
 @testset begin
     test_3x3_lt()
     test_4x4_blt()
+    test_nn_jacobian()
 end
