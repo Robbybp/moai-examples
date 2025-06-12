f87effced08bb14e6dfc0bffc5c386d1d8fdd4d1
Robert Parker <robbybparker@gmail.com>
2025-06-12 12:24:44 -0400

    test kkt pivot matrix from NN

--- mnist/test-btsolver.jl
+++ mnist/test-btsolver.jl
@@ -11,6 +11,7 @@ using Printf
 include("btsolver.jl")
 include("models.jl")
 include("nlpmodels.jl")
+include("linalg.jl")
 
 function _test_matrix(csc::SparseArrays.SparseMatrixCSC; atol=1e-8)
     csc = SparseArrays.sparse(csc)
@@ -81,9 +82,31 @@ function test_nn_jacobian()
     return
 end
 
+function test_nn_kkt()
+    model, info = make_small_nn_model()
+    nlp, kkt_system, kkt_matrix = get_kkt(model)
+    pivot_indices = get_kkt_indices(model, info.variables, info.constraints)
+    kkt_matrix = fill_upper_triangle(kkt_matrix)
+    pivot_dim = length(pivot_indices)
+    pivot_matrix = kkt_matrix[pivot_indices, pivot_indices]
+
+    # Filter out constraint regularization nonzeros
+    # By convention, constraints are the second half of the pivot indices
+    to_ignore = Set(Int(pivot_dim / 2 + 1):pivot_dim)
+    I, J, V = SparseArrays.findnz(pivot_matrix)
+    to_retain = filter(k -> !(I[k] in to_ignore && J[k] in to_ignore), 1:length(I))
+    I = I[to_retain]
+    J = J[to_retain]
+    V = V[to_retain]
+    pivot_matrix = SparseArrays.sparse(I, J, V, pivot_dim, pivot_dim)
+
+    _test_matrix(pivot_matrix)
+end
+
 @testset begin
     test_3x3_lt()
     test_3x3_lt_unsym_perm()
     test_4x4_blt()
     test_nn_jacobian()
+    test_nn_kkt()
 end
