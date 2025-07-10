03da81bbff2f28622ee111d387f8f78d34a14e68
Robert Parker <robbybparker@gmail.com>
2025-07-10 11:40:32 -0400

    script for generating pprof viz

---
+++ mnist/view-profile.jl
@@ -0,0 +1,10 @@
+using Serialization
+import PProf
+# Modules get serialized, so I need to import them before I deserialize...
+import JuMP
+import MathProgIncidence
+import MathOptAI
+allocdata = open("allocdata.bin", "r") do io
+    return deserialize(io)
+end
+PProf.Allocs.pprof(allocdata)
