11ceb13e59add7cce1c70b3a241c3ee39ebce031
Robert Parker <robbybparker@gmail.com>
2025-09-03 17:12:57 -0400

    sweep params

--- isti2025/nn-getter.jl
+++ isti2025/nn-getter.jl
@@ -1,18 +1,26 @@
 MODEL_TO_NNS = Dict(
     "mnist" => [
-        "mnist-relu128nodes4layers.pt",
-        "mnist-relu512nodes4layers.pt",
-        "mnist-relu1024nodes4layers.pt",
-        "mnist-relu2048nodes4layers.pt",
+        #"mnist-tanh128nodes4layers.pt",
+        "mnist-tanh512nodes4layers.pt",
+        "mnist-tanh1024nodes4layers.pt",
+        "mnist-tanh2048nodes4layers.pt",
+        #"mnist-relu128nodes4layers.pt",
+        #"mnist-relu512nodes4layers.pt",
+        #"mnist-relu1024nodes4layers.pt",
+        #"mnist-relu2048nodes4layers.pt",
         #"mnist-relu4096nodes4layers.pt",
         #"mnist-sigmoid8192nodes4layers.pt",
     ],
     "scopf" => [
-        joinpath("scopf", "100nodes3layers.pt"),
+        #joinpath("scopf", "100nodes3layers.pt"),
         joinpath("scopf", "500nodes5layers.pt"),
         joinpath("scopf", "1000nodes7layers.pt"),
+        joinpath("scopf", "1500nodes10layers.pt"),
         #joinpath("scopf", "2000nodes20layers.pt"),
         #joinpath("scopf", "4000nodes40layers.pt"),
+        #joinpath("scopf", "relu100nodes3layers.pt"),
+        #joinpath("scopf", "relu500nodes5layers.pt"),
+        #joinpath("scopf", "relu1000nodes7layers.pt"),
     ],
 )
 
